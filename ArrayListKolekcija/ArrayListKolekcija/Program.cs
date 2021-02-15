7c0e9537e8c76ebaec84576bf4fba687ea772b39
Neven <58472164+neven14@users.noreply.github.com>
2021-02-15 08:52:29 +0100

    Add files via upload

---
+++ ArrayListKolekcija/ArrayListKolekcija/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListKolekcija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.ArrayList al = new System.Collections.ArrayList();
+            al.Add(4);
+            al.Add("C# i .NET");
+
+            al.Insert(1, "Programiranje");
+
+            Console.WriteLine("--Elementi kolekcije na početku:");
+
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+           
+            if (al.Contains(4))
+            {
+                al.Remove(4);
+            }
+            int pozicija = al.IndexOf("Programiranje");
+            if (pozicija >= 0)
+            {
+                al.RemoveAt(pozicija);
+            }
+            Console.WriteLine("\n-- Elementi kolekcije na završetku:");
+
+
+
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
